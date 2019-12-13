73421a061b57904e581b3d4df9f791dd577338b5
Abhijeet Mulgund <aam14@rice.edu>
2019-12-12 23:25:43 -0800

    first commit

---
+++ .scripts/startup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+feh --bg-fill $HOME/Pictures/normandy_destruction_wallpaper_clean_4k_16_9.jpg 
+
+sh $HOME/.screenlayout/room.sh
+
---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Setup and upgrade
+sudo apt update
+sudo apt dist-upgrade
+
+# Install i3wm
+sudo apt install i3
+
+# Install applets
+sudo apt install network-manager
+sudo apt install blueman
+
